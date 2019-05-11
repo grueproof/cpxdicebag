d5772e66faf7ca8e0ea1777caf96f4fb83dee462
Chris Miller <ctmiller@pm.me>
2019-05-11 15:54:02 -0400

    Discovered the cpx module and refactored to make good use of it.

--- code.py
+++ code.py
@@ -1,10 +1,7 @@
-import audioio
-import board
-import digitalio
-import neopixel
 import random
 import math
 import time
+from adafruit_circuitplayground.express import cpx
 
 # Colors
 RED = 0x100000  # (0x10, 0, 0) also works
@@ -14,25 +11,6 @@ AQUA = (0, 0x10, 0x10)
 BLUE = (0, 0, 0x10)
 PURPLE = (0x10, 0, 0x10)
 BLACK = (0, 0, 0)
-
-# init the neopixels
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=.2)
-pixels.fill((0, 0, 0))
-pixels.show()
-
-# enable the speaker
-spkrenable = digitalio.DigitalInOut(board.SPEAKER_ENABLE)
-spkrenable.direction = digitalio.Direction.OUTPUT
-spkrenable.value = True
-
-# make the 2 input buttons
-buttonA = digitalio.DigitalInOut(board.BUTTON_A)
-buttonA.direction = digitalio.Direction.INPUT
-buttonA.pull = digitalio.Pull.DOWN
-
-buttonB = digitalio.DigitalInOut(board.BUTTON_B)
-buttonB.direction = digitalio.Direction.INPUT
-buttonB.pull = digitalio.Pull.DOWN
 
 def wheel(pos):
     # Input a value 0 to 255 to get a color value.
@@ -54,33 +32,14 @@ def rainbow_cycle(wait):
         pixels.show()
         time.sleep(wait)
 
-def play_file(filename):
-    print("Playing file: " + filename)
-    wave_file = open(filename, "rb")
-    with audioio.WaveFile(wave_file) as wave:
-        with audioio.AudioOut(board.A0) as audio:
-            audio.play(wave)
-            while audio.playing:
-                pass
-    print("Finished")
-
 def roll(max,times=1, each_roll=0,end_add=0):
-    play_file('diceroll.wav')
+    cpx.play_file('diceroll.wav')
     total = 0
     for index in range(0,times):
         total += random.randint(1, max) + each_roll
     return total
 
-def init_pixels():
-    pixels.fill((0, 0, 0))
-    pixels.show()
-
-def roll_to_hit():
-    init_pixels()
-    total = roll(20)
-
-    print("Total: %s" % total)
-
+def display_roll_value(total):
     # do the math for showing a red light for 10, and green lights for 1
     tens = math.floor(total/10)
     remain = total%10
@@ -92,18 +51,33 @@ def roll_to_hit():
         else:
             pixels[i]=GREEN
 
+def init_pixels():
+    pixels.fill((0, 0, 0))
+    pixels.show()
+
+def roll_to_hit():
+    init_pixels()
+    total = roll(20)
+
+    print("Total: %s" % total)
+
+    display_roll_value(total)
+
     if total==1:
         pixels.fill(RED)
-        play_file("laugh.wav")
+        cpx.play_file("laugh.wav")
 
     if total==20:
-        play_file("fanfare.wav")
+        cpx.play_file("fanfare.wav")
         rainbow_cycle(.001)
 
-
+# init the neopixels
+pixels = cpx.pixels
+pixels.fill((0, 0, 0))
+pixels.show()
 
 while True:
-    if buttonA.value:
+    if cpx.button_a:
         roll_to_hit()
-    if buttonB.value:
+    if cpx.button_b:
         init_pixels()
